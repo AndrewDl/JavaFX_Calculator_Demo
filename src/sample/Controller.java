58e96ad1c998acd85ef71da8e490a4de6630c575
AndrewDl <dlandrew@ukr.net>
2016-08-02 01:31:10 +0300

    Calculator MVC v1.1
    added model for calculator

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,62 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public TextField resultWindow;
+
+    public void onPadClick(ActionEvent actionEvent) {
+        String currentValue = resultWindow.getText();
+
+        if (actionEvent.getSource().getClass() == Button.class)
+        {
+            Button button = (Button)actionEvent.getSource();
+            String buttonValue = button.getText();
+
+            resultWindow.setText(currentValue+buttonValue);
+
+        }
+    }
+
+    public void onActonClick(ActionEvent actionEvent) {
+        Button button = null;
+
+        if (actionEvent.getSource().getClass() == Button.class)
+        {
+            button = (Button)actionEvent.getSource();
+            String buttonValue = button.getText();
+
+        }
+
+        switch (button.getText()){
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            case "=":
+                break;
+            case "C":
+                resultWindow.setText("");
+                break;
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void onCalculateClick(ActionEvent actionEvent) {
+    }
 }
---
+++ src/sample/Model.java
@@ -0,0 +1,23 @@
+package sample;
+
+/**
+ * Created by Andrew on 02.08.2016.
+ */
+public class Model {
+    static double Sum(double a, double b){
+
+        return a+b;
+    }
+
+    static double Subtract(double a, double b){
+        return a-b;
+    }
+
+    static double Multiply(double a, double b){
+        return a*b;
+    }
+
+    static double Divide(double a, double b){
+        return a/b;
+    }
+}
